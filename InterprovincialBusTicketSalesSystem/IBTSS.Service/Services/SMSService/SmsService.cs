a33302d4cac78c8460d9bafe8857c5a649d56722
Trần Minh Trí <tritmse173635@fpt.edu.vn>
2025-04-22 16:05:33 +0700

    Add_SMSService

---
+++ InterprovincialBusTicketSalesSystem/IBTSS.Service/Services/SMSService/SmsService.cs
@@ -0,0 +1,31 @@
+﻿using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+
+namespace IBTSS.Service.Services.SMSService
+{
+   
+    public class SmsService
+    {
+        private const string AccountSid = "AC4c8412280f811dc09ba2b8cfb84a64f6";
+        private const string AuthToken = "861ddb2b2a51fce2f4647a0e2e5e6848";
+        private const string FromPhone = "+19712932825";
+
+        public void SendBookingConfirmation(string customerPhone, string customerName, string tripInfo)
+        {
+            TwilioClient.Init(AccountSid, AuthToken);
+
+            var messageBody = $"Chào {customerName}, bạn đã đặt vé thành công cho chuyến đi: {tripInfo}. Cảm ơn bạn!";
+
+            var message = MessageResource.Create(
+                body: messageBody,
+                from: new PhoneNumber(FromPhone),
+                to: new PhoneNumber(customerPhone)
+            );
+
+            Console.WriteLine($"Tin nhắn gửi thành công! SID: {message.Sid}");
+        }
+    }
+
+}
